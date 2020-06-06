bdf3777e851a166f79aa56b2aea67a4e584593a9
Yimni Chan <ynchan818@gmail.com>
2020-06-06 15:07:34 -0400

    add constrictor

--- src/components/Table.js
+++ src/components/Table.js
@@ -1,8 +1,21 @@
 import React, { Component } from "react";
 
+import TableRow from "./TableRow"
 
 
-class Tabl extends Component {
+class Table extends Component {
+    constructor(props){
+        super(props)
+        this.state={
+            //color:"",
+            row:this.props.row,
+            column:this.props.row
+        }
+        this.handleAddEvent = this.handleAddEvent.bind(this);
+      }
+
+
+   // <table id="table"></table>
 
 }
 export default Table;
